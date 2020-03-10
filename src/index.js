512f08c64c63b03bdafd03c266b918d0c241effa
iashchuk <221box@list.ru>
2020-03-10 22:54:19 +0300

    adds format option

--- src/index.js
+++ src/index.js
@@ -5,5 +5,10 @@ const program = new commander.Command();
 export default () => {
     program.version("1.0.0");
     program.description("Compares two configuration files and shows a difference.");
+    program.option("-f, --format [type]", "output format");
+    program.arguments("<firstConfig> <secondConfig>");
+    program.action((firstConfig, secondConfig) => {
+        console.log(firstConfig, secondConfig);
+    });
     program.parse(process.argv);
 };
