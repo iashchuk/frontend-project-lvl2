73323ffcb97262d1289221cc6b0f18fe9f3d49ed
iashchuk <221box@list.ru>
2020-03-16 23:10:01 +0300

    adds parse factory

---
+++ src/parsers/index.js
@@ -0,0 +1,24 @@
+import path from 'path';
+import fs from 'fs';
+import JsonParser from './JsonParser';
+import YamlParser from './YamlParser';
+import IniParser from './IniParser';
+
+const mapping = {
+  yaml: YamlParser,
+  yml: YamlParser,
+  json: JsonParser,
+  ini: IniParser,
+};
+
+export default class ParseFactory {
+  static factory(filePath) {
+    const type = path.extname(filePath).slice(1);
+    const rawData = fs.readFileSync(filePath).toString();
+
+    const parser = new mapping[type](rawData);
+    const data = parser.parse();
+
+    return data;
+  }
+}
