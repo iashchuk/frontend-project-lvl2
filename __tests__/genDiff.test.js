48c36203a067e273e0615ffb777c2a8c3d9ff0e2
iashchuk <221box@list.ru>
2020-03-16 23:05:24 +0300

    adds tests

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { genDiff } from '../src/genDiff';
+
+const formats = ['json', 'yml', 'yaml', 'ini'];
+const getFixturePath = (name) => path.resolve(__dirname, '__fixtures__', name);
+
+let expected;
+
+beforeAll(() => {
+  expected = fs
+    .readFileSync(getFixturePath('result.txt'), 'utf-8')
+    .split('\n')
+    .map((item) => item.trim())
+    .join('\n');
+});
+
+test.each(formats)('%s', (format) => {
+  const pathToFile1 = getFixturePath(`before.${format}`);
+  const pathToFile2 = getFixturePath(`after.${format}`);
+  const actual = genDiff(pathToFile1, pathToFile2);
+  expect(actual).toEqual(expected.trim());
+});
---
+++ __tests__/renderDiff.test.js
@@ -0,0 +1,49 @@
+import { renderDiff } from '../src/genDiff';
+
+describe('renderDiff', () => {
+  it('test added element', () => {
+    const addedElement = {
+      type: 'added',
+      key: 'verbose',
+      values: [undefined, true],
+    };
+
+    expect(renderDiff(addedElement)).toBe(`+ ${addedElement.key}: ${addedElement.values[1]}`);
+  });
+  it('test removed element', () => {
+    const removedElement = {
+      type: 'removed',
+      key: 'proxy',
+      values: ['123.234.53.22', undefined],
+    };
+    expect(renderDiff(removedElement)).toBe(`- ${removedElement.key}: ${removedElement.values[0]}`);
+  });
+  it('test changed element', () => {
+    const changedElement = {
+      type: 'changed',
+      key: 'timeout',
+      values: [50, 20],
+    };
+    expect(renderDiff(changedElement)).toBe(
+      `- ${changedElement.key}: ${changedElement.values[0]}\n+ ${changedElement.key}: ${changedElement.values[1]}`,
+    );
+  });
+  it('test unchanged element', () => {
+    const unchangedElement = {
+      type: 'unchanged',
+      key: 'host',
+      values: ['hexlet.io', 'hexlet.io'],
+    };
+    expect(renderDiff(unchangedElement)).toBe(
+      `${unchangedElement.key}: ${unchangedElement.values[0]}`,
+    );
+  });
+  it('test unknown element', () => {
+    const errorElement = {
+      type: 'unchged',
+      key: 'host',
+      values: ['hexlet.io', 'hexlet.io'],
+    };
+    expect(() => renderDiff(errorElement)).toThrow(`Unknown type: ${errorElement.type}`);
+  });
+});
