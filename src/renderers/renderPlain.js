49db5d83c27300c6c1a8f7fe7a11f85297741e8d
iashchuk <221box@list.ru>
2020-04-13 23:57:00 +0300

    fixes render plain

--- src/renderers/renderPlain.js
+++ src/renderers/renderPlain.js
@@ -1,37 +1,42 @@
 // @ts-check
 import { isString, isObject } from 'lodash';
 
-
 const renderPrimitive = (item) => (isString(item) ? `'${item}'` : item);
 
 const formatValues = (values) => values.map((value) => (isObject(value) ? '[complex value]' : renderPrimitive(value)));
 
 
-export const renderDiff = (element, initPath = '') => {
-  const path = [initPath, element.key].filter(isString).join('.');
+export const renderDiff = (diff, initPath = '') => {
+  const iter = (element) => {
+    const path = [initPath, element.key].filter(Boolean).join('.');
 
-  if (element.type === 'nested') {
-    return element.children.map((item) => renderDiff(item, path)).filter(Boolean).join('\n');
-  }
+    if (element.type === 'nested') {
+      return renderDiff(element.children, path);
+    }
 
-  const [firstValue, secondValue] = formatValues(element.values);
+    const [firstValue, secondValue] = formatValues(element.values);
 
-  switch (element.type) {
-    case 'removed':
-      return `Property '${path}' was removed`;
+    switch (element.type) {
+      case 'removed':
+        return `Property '${path}' was removed`;
 
-    case 'added':
-      return `Property '${path}' was added with value: ${secondValue}`;
+      case 'added':
+        return `Property '${path}' was added with value: ${secondValue}`;
 
-    case 'changed':
-      return `Property '${path}' was updated. From ${firstValue} to ${secondValue}`;
+      case 'changed':
+        return `Property '${path}' was updated. From ${firstValue} to ${secondValue}`;
 
-    case 'unchanged':
-      return '';
+      case 'unchanged':
+        return '';
 
-    default:
-      throw new Error(`Unknown type: ${element.type}`);
-  }
+      default:
+        throw new Error(`Unknown type: ${element.type}`);
+    }
+  };
+
+  return diff.map(iter).filter(Boolean).join('\n');
 };
 
-export default (diff) => diff.map(renderDiff).filter(Boolean).join('\n');
+const renderPlain = (diff) => renderDiff(diff);
+
+export default renderPlain;
