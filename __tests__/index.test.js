1b39f0291b1bbe9bd0fb65b1069966c2f0479824
iashchuk <221box@list.ru>
2020-04-16 01:27:57 +0300

    updates tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,11 +3,6 @@ import path from 'path';
 import genDiff from '../src';
 
 const formats = ['json', 'yaml', 'yml', 'ini'];
-const renderers = {
-  pretty: 'pretty',
-  plain: 'plain',
-  json: 'json',
-};
 
 const getFixturePath = (name) => path.resolve(__dirname, '__fixtures__', name);
 
@@ -26,15 +21,15 @@ describe('should be work correct', () => {
   let expectedJson;
 
   beforeAll(() => {
-    expectedPretty = getResult(renderers.pretty);
-    expectedPlain = getResult(renderers.plain);
-    expectedJson = getResult(renderers.json);
+    expectedPretty = getResult('pretty');
+    expectedPlain = getResult('plain');
+    expectedJson = getResult('json');
   });
 
   test.each(formats)('format: %s', (format) => {
     const { pathToFile1, pathToFile2 } = getFilePaths(format);
-    expect(genDiff(pathToFile1, pathToFile2, renderers.pretty)).toEqual(expectedPretty);
-    expect(genDiff(pathToFile1, pathToFile2, renderers.plain)).toEqual(expectedPlain);
-    expect(genDiff(pathToFile1, pathToFile2, renderers.json)).toEqual(expectedJson);
+    expect(genDiff(pathToFile1, pathToFile2, 'pretty')).toEqual(expectedPretty);
+    expect(genDiff(pathToFile1, pathToFile2, 'plain')).toEqual(expectedPlain);
+    expect(genDiff(pathToFile1, pathToFile2, 'json')).toEqual(expectedJson);
   });
 });
