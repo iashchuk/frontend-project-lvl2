9cd3da99332eda34900157ff15b706575a70910f
iashchuk <221box@list.ru>
2020-04-28 23:10:07 +0300

    fixes render diff

--- src/renderers/renderPlain.js
+++ src/renderers/renderPlain.js
@@ -12,11 +12,11 @@ const formatValue = (value) => {
   return value;
 };
 
-export const renderDiff = (diff, basePath = []) => {
+export const renderDiff = (diff, ancestors = []) => {
   const iter = ({
     key, type, firstValue, secondValue, children,
   }) => {
-    const path = [...basePath, key];
+    const path = [...ancestors, key];
     const value1 = formatValue(firstValue);
     const value2 = formatValue(secondValue);
 
