8d7901bd2de65ae7363a9cacbb8526855203231d
iashchuk <221box@list.ru>
2020-04-29 00:17:33 +0300

    translates common files to typescript

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,23 +2,23 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-const formats = ['json', 'yaml', 'yml', 'ini'];
+const formats: string[] = ['json', 'yaml', 'yml', 'ini'];
 
-const getFixturePath = (name) => path.resolve(__dirname, '__fixtures__', name);
+const getFixturePath = (name: string) => path.resolve(__dirname, '__fixtures__', name);
 
-const getFilePaths = (format) => {
+const getFilePaths = (format: string) => {
   const pathToFile1 = getFixturePath(`before.${format}`);
   const pathToFile2 = getFixturePath(`after.${format}`);
   return { pathToFile1, pathToFile2 };
 };
 
 
-const getResult = (format) => fs.readFileSync(getFixturePath(`result.${format}.txt`), 'utf-8');
+const getResult = (format: string) => fs.readFileSync(getFixturePath(`result.${format}.txt`), 'utf-8');
 
 describe('should be work correct', () => {
-  let expectedPretty;
-  let expectedPlain;
-  let expectedJson;
+  let expectedPretty: string;
+  let expectedPlain: string;
+  let expectedJson: string;
 
   beforeAll(() => {
     expectedPretty = getResult('pretty');
